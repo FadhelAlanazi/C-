e0834c8e027d3cc2277e6402dde8730393b14fb1
FadhelAlanazi <fadheldev@hotmail.com>
2023-06-29 14:23:44 +0300

    Adding Class-Templete

---
+++ Class-Templete/Class-Templete/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  Class-Templete
+//
+//  Created by Fadhel Alanazi on 08/12/1444 AH.
+//
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Student{
+    T x;
+public:
+    Student(T x){
+        this->x = x;
+    }
+    
+    void print(){
+        cout<< "x value: "<<x<<endl;
+    }
+};
+
+
+int main(int argc, const char * argv[]) {
+    
+    Student s("Ahmad");
+    s.print();
+  
+    return 0;
+}
