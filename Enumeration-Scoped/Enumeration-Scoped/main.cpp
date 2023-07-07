c57b13d3053d09a58a6047423a0379e25a32c920
FadhelAlanazi <fadheldev@hotmail.com>
2023-07-07 15:11:26 +0300

    Scoped enums added

---
+++ Enumeration-Scoped/Enumeration-Scoped/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  Enumeration-Scoped
+//
+//  Created by Fadhel Alanazi on 19/12/1444 AH.
+//
+// 2.scoped enums - do not leak value into an outer scope
+
+#include <iostream>
+using namespace std;
+
+enum class Weekdays{
+    Saturday,
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday
+};
+
+int main(int argc, const char * argv[]) {
+   
+    Weekdays day = Weekdays::Saturday;
+    
+    return 0;
+}
